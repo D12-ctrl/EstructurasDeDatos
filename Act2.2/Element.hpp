2015d56e68fa4e4d3c35ec7ac1644a7876398407
D12-ctrl <a01655451@itesm.mx>
2021-10-05 19:36:28 -0500

    Clase Element

---
+++ Act2.2/Element.hpp
@@ -0,0 +1,39 @@
+/*
+    Element.hpp
+    Actividad 2.1
+
+    Diego Mellado Oliveros
+    Eduardo Gaona 
+*/
+
+#include <iostream>
+#include <string>
+
+#pragma once
+
+using namespace std;
+
+template <class T>
+class Element
+{
+    protected:
+        T information;
+        Element<T>* next = nullptr;
+
+    public:
+            //Constructores
+        Element(){}
+        Element(const T & info): information(info){}
+
+            //Destructor
+        //virtual ~Element();
+
+            //Setter and getters
+        Element<T>* getNextElement() const {return next;}
+        void setNextElement(Element<T>* elemnt){next = elemnt;}
+        void print(){cout << information << endl;}
+        T getInfo() const{return information;}
+
+
+};
+
