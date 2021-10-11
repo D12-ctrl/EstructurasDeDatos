a77dc9481276a9efa2c471d8249761cf0a5be5c9
Eduardo Gaona <eduardogaona@MacBook-Pro-de-Eduardo.local>
2021-10-11 12:38:40 -0500

    Lalo

--- Act2.2/Element.hpp
+++ Act2.2/Element.hpp
@@ -1,9 +1,12 @@
 /*
+ITESM
+
     Element.hpp
     Actividad 2.1
 
+
     Diego Mellado Oliveros
-    Eduardo Gaona 
+    Eduardo Gaona Romero
 */
 
 #include <iostream>
@@ -16,24 +19,21 @@ using namespace std;
 template <class T>
 class Element
 {
-    protected:
-        T information;
-        Element<T>* next = nullptr;
+protected:
+    T information;
+    Element<T> *next = nullptr;
 
-    public:
-            //Constructores
-        Element(){}
-        Element(const T & info): information(info){}
+public:
+    //Constructores
+    Element() {}
+    Element(const T &info) : information(info) {}
 
-            //Destructor
-        //virtual ~Element();
+    //Destructor
+    //virtual ~Element();
 
-            //Setter and getters
-        Element<T>* getNextElement() const {return next;}
-        void setNextElement(Element<T>* elemnt){next = elemnt;}
-        void print(){cout << information << endl;}
-        T getInfo() const{return information;}
-
-
+    //Setter and getters
+    Element<T> *getNextElement() const { return next; }
+    void setNextElement(Element<T> *elemnt) { next = elemnt; }
+    void print() { cout << information << endl; }
+    T getInfo() const { return information; }
 };
-
