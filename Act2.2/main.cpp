cfb3a68cebc49b6fb1f40fb0317c161c51737000
Eduardo Gaona <eduardogaona@MacBook-Pro-de-Eduardo.local>
2021-10-11 21:10:04 -0500

    MAIN

--- Act2.2/main.cpp
+++ Act2.2/main.cpp
@@ -1,1 +1,29 @@
+/* 
 
+Instituto Tecnológico y de Estudios Superiores de Monterrey 
+
+Programación de estructuras y algoritmos fundamentales
+
+Equipo # 12
+
+Diego Mellado Oliveros
+Eduardo Gaona Romero 
+
+Act 2.2
+Actividad Integral estructura de datos lineales 
+
+Profesor: Vicente Cubells
+
+Última modificación Mon, 11th, Oct, 2021. 
+
+*/
+//Herencia de los header
+#include "Element.hpp"
+#include "ConexionesComputadoras.hpp"
+
+int main()
+{
+    cout << "Buen día, el siguiente programa ayuda a la gestión del csv revisado en la evidencia anterior" << endl;
+
+    return 0;
+}
